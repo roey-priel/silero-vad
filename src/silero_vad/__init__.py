2069e4210518ee8e8ad59bbf579546cb0cad17f0
roey-priel <roey@tavus.io>
2024-12-16 18:44:35 +0000

    python3.13 support

--- src/silero_vad/__init__.py
+++ src/silero_vad/__init__.py
@@ -6,7 +6,5 @@ except:
 
 from silero_vad.model import load_silero_vad
 from silero_vad.utils_vad import (get_speech_timestamps,
-                                  save_audio,
-                                  read_audio,
                                   VADIterator,
                                   collect_chunks)
